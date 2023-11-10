adce8aa887b06a2875c15b322ef26e2d9cc92b8c
you277 <yuxuanl7@nycstudents.net>
2023-11-10 15:29:59 -0500

    dfgsdfbdfgh

--- Loops.java
+++ Loops.java
@@ -1,15 +1,14 @@
 public class Loops {
-    public static int numDigits(int num) {
+    public int numDigits(int num) {
         return String.valueOf(num).length();
     }
 
-    public static boolean isDivisible(int a, int b) {
+    public boolean isDivisible(int a, int b) {
         return a%b == 0;
     }
-
-    public static int subOdd(int floor, int ceiling) {
+    public int sumOddBetween(int floor, int ceiling) {
         int result = 0;
-        for (int i = floor; i < ceiling; i++) {
+        for (int i = floor; i <= ceiling; i++) {
             if (i%2 == 1) {
                 result += i;
             }
@@ -17,7 +16,7 @@ public class Loops {
         return result;
     }
 
-    public static int sumMult10(int floor, int ceiling) {
+    public int sumMult10(int floor, int ceiling) {
         int result = 0;
         for (int i = floor; i < ceiling; i++) {
             if (i%10 == 0) {
@@ -27,7 +26,7 @@ public class Loops {
         return result;
     }
 
-    public static int sumDigits(int num) {
+    public int sumDigits(int num) {
         String str = String.valueOf(num);
         int length = str.length();
         int result = 0;
@@ -37,26 +36,53 @@ public class Loops {
         return result;
     }
 
-    public static int max2PowerLess(int num) {
+    public int max2PowerLess(int num) {
         int result = 2;
         while (true) {
             int nextResult = result*2;
             if (nextResult >= num) {
-                return nextResult;
+                return result;
             }
             result = nextResult;
         }
     }
 
-    public static int numOccurences(String str, String pattern) {
-        int occurences = 0;
+    public int numbOccurrences(String str, String pattern) {
+        int occurrences = 0;
+        int patternLength = pattern.length();
         while (true) {
             int location = str.indexOf(pattern);
             if (location == -1) {
-                return occurences;
+                return occurrences;
             }
-            occurences++;
-            str = str.substring(location);
+            occurrences++;
+            str = str.substring(location + patternLength);
         }
+    }
+
+    public Loops() {
+        System.out.println("1: numDigits");
+        System.out.print("| ");System.out.println(numDigits(123));
+        System.out.print("| ");System.out.println(numDigits(2147483647));
+        System.out.println("2: isDivisible");
+        System.out.print("| ");System.out.println(isDivisible(12, 3));
+        System.out.print("| ");System.out.println(isDivisible(546357653, 7));
+        System.out.print("| ");System.out.println(isDivisible(20, 3));
+        System.out.println("3: sumOddBetween");
+        System.out.print("| ");System.out.println(sumOddBetween(1, 10));
+        System.out.print("| ");System.out.println(sumOddBetween(5, 10));
+        System.out.print("| ");System.out.println(sumOddBetween(10, 20));
+        System.out.println("4: sumMult10");
+        System.out.print("| ");System.out.println(sumMult10(5, 25));
+        System.out.print("| ");System.out.println(sumMult10(10, 30));
+        System.out.println("5: sumDigits");
+        System.out.print("| ");System.out.println(sumDigits(234));
+        System.out.print("| ");System.out.println(sumDigits(2147483647));
+        System.out.println("6: max2PowerLess");
+        System.out.print("| ");System.out.println(max2PowerLess(100));
+        System.out.print("| ");System.out.println(max2PowerLess(65535));
+        System.out.println("7: numbOccurrences");
+        System.out.print("| ");System.out.println(numbOccurrences("HELLO HELLO!!!", "HELL"));
+        System.out.print("| ");System.out.println(numbOccurrences("hello everybody my name is markiplier and today we will be playing", "e"));
     }
 }
